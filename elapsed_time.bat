246093c5a22f49d09805cd3675f246ff57090582
ahornero <ahornero@users.noreply.github.com>
2017-06-07 13:54:21 +0200

    Add files via upload

---
+++ elapsed_time.bat
@@ -0,0 +1,23 @@
+@echo off
+
+rem Get start time:
+for /F "tokens=1-4 delims=:.," %%a in ("%time%") do (
+   set /A "start=(((%%a*60)+1%%b %% 100)*60+1%%c %% 100)*100+1%%d %% 100"
+)
+
+L2A_Process S2A_OPER_PRD_MSIL1C_PDMC_20160629T172303_R036_V20160629T094727_20160629T094727.SAFE
+
+rem Get end time:
+for /F "tokens=1-4 delims=:.," %%a in ("%time%") do (
+   set /A "end=(((%%a*60)+1%%b %% 100)*60+1%%c %% 100)*100+1%%d %% 100"
+)
+
+rem Get elapsed time:
+set /A elapsed=end-start
+
+rem Show elapsed time:
+set /A hh=elapsed/(60*60*100), rest=elapsed%%(60*60*100), mm=rest/(60*100), rest%%=60*100, ss=rest/100, cc=rest%%100
+if %mm% lss 10 set mm=0%mm%
+if %ss% lss 10 set ss=0%ss%
+if %cc% lss 10 set cc=0%cc%
+echo Elapsed: %hh%:%mm%:%ss%,%cc%
